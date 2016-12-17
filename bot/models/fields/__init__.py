5c08b9b4803c72a9571d04668f5188b47156d5b6
Savva Surenkov <savva.surenkov@outlook.com>
2016-12-18 00:23:59 +1000

    Attempt to fix serialized field

--- bot/models/fields/__init__.py
+++ bot/models/fields/__init__.py
@@ -5,9 +5,11 @@ from django.db import models
 class SerializedField(models.BinaryField):
 
     def to_python(self, value):
+        if value is None:
+            return None
         return pickle.loads(super(SerializedField, self).to_python(value))
 
     def get_prep_value(self, value):
-        bytes_value = pickle.dumps(value)
+        bytes_value = pickle.dumps(value) if value is not None else None
         return super(SerializedField, self).get_prep_value(bytes_value)
 
