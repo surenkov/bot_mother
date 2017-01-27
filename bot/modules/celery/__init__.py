077550af80f6d54987956957c3e501ebbe45a686
Savva Surenkov <savva.surenkov@outlook.com>
2017-01-27 14:11:57 +1000

    Fix task result serialization error

--- bot/modules/celery/__init__.py
+++ bot/modules/celery/__init__.py
@@ -15,8 +15,9 @@ app.conf.enable_utc = settings.USE_TZ
 app.conf.timezone = settings.TIME_ZONE
 
 # MQ interfaces
-app.conf.accept_content = ['pickle']
+app.conf.accept_content = {'pickle'}
 app.conf.task_serializer = 'pickle'
+app.conf.result_serializer = 'pickle'
 
 app.autodiscover_tasks()
 
